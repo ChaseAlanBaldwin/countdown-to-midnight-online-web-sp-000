70f44da67960bd4e31fba613797052e399c0e638
Chase-Alan Baldwin <alan.chasebaldwin@gmail.com>
2020-10-30 00:33:56 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,10 @@
 #write your code here
 
 def countdown(count)
-  count += 1
+
   while count > 0 do
     count -= 1
-    puts "#{count} SECOND(S)"
+    puts "#{count} SECOND(S)!"
   end
   "HAPPY NEW YEAR!"
 end
