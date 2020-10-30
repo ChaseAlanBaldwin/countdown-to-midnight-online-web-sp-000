208e3c3b02ce44b8a1b28f5e18c330b36f85ebb5
Chase-Alan Baldwin <alan.chasebaldwin@gmail.com>
2020-10-30 00:35:09 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,9 +2,9 @@
 
 def countdown(count)
   while count > 0 do
-    if count == 0
-      break
-    end
+      if count == 0
+        break
+      end
     count -= 1
     puts "#{count} SECOND(S)!"
   end
